0844806c043c78392516f6e08469378b3a55940f
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-04-21 11:56:02 -0500

    Form correctly gets user stat

--- src/index.js
+++ src/index.js
@@ -4,24 +4,47 @@ import './index.css';
 import ReactDOM from 'react-dom'
 
 class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+    /* https://fortnite-api.com/v1/stats/br/v2?name=Guyfierimustdie&time=lifetime */
+  handleSubmit(event) {
+    alert('Name: ' + this.state.value);
+    event.preventDefault();
+    (async () => {
+      const response = await axios.get('https://fortnite-api.com/v1/stats/br/v2',
+        {params: {name: `${this.state.value}`}}
+      );
+        
+      
+    
+      console.log(response)
+    })()
+  }
+  
   render() {
     return (
-      <div className="stat-block">
-        yo
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
 
-;(async () => {
-  const response = await axios({
-    url: 'https://fortnite-api.com/v1/stats/br/v2?name=Guyfierimustdie&time=lifetime',
-    method: 'get'
-  })
-
-  console.log(response)
-})()
 
 
 
